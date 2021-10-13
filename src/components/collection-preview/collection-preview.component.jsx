ee2b7e037529ccffbd2a20c7cb7bf84cf2c6b784
williamchavula <rumbani@gmail.com>
2021-10-13 14:37:35 -0500

    add cart.utils.js to group cart items

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -7,8 +7,8 @@ const CollectionPreviewComponent = ({ title, items }) => (
         <h1 className='title'>{ title.toUpperCase() }</h1>
         <div className="preview">
             {
-                items.filter((item, idx) => idx < 4).map(({ id, ...itemProps }) => (
-                    <CollectionItemComponent key={id} {...itemProps } />
+                items.filter((item, idx) => idx < 4).map(item => (
+                    <CollectionItemComponent key={item.id} item={item} />
                 ))
             }
         </div>
