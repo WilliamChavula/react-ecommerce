b7e99629e530bae81e3585283446100b3240d8d2
williamchavula <rumbani@gmail.com>
2021-10-19 16:59:14 -0500

    improve custom-button.styles.js to render properly on collection-item.component.jsx

--- src/components/custom-buttom/custom-button.component.jsx
+++ src/components/custom-buttom/custom-button.component.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 
 import './custom-button.styles.scss'
-// import { CustomButtonContainer } from './custom-button.styles'
+import { CustomButtonContainer } from './custom-button.styles'
 
-// const CustomButtonComponent = ({ children, ...otherProps }) => (
-//     <CustomButtonContainer { ...otherProps }>
-//         { children }
-//     </CustomButtonContainer>
-// );
-
-const CustomButtonComponent = ({ children, isGoogleSignIn, inverted, ...otherProps }) => (
-    <button className={`${inverted ? 'inverted' : ''} ${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`} { ...otherProps }>
+const CustomButtonComponent = ({ children, ...otherProps }) => (
+    <CustomButtonContainer { ...otherProps }>
         { children }
-    </button>
+    </CustomButtonContainer>
 );
+
+// const CustomButtonComponent = ({ children, isGoogleSignIn, inverted, ...otherProps }) => (
+//     <button className={`${inverted ? 'inverted' : ''} ${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`} { ...otherProps }>
+//         { children }
+//     </button>
+// );
 
 
 
